7462da8dab1f29326e33c42fab920ff53ad9e42e
manish-mehra <manishmehra1469@gmail.com>
2024-04-13 11:17:12 +0530

    refactor: handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,13 +1,10 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/manish-mehra/go-todo/models"
-	"github.com/manish-mehra/go-todo/services"
 	"github.com/manish-mehra/go-todo/utils"
 )
 
@@ -19,8 +16,8 @@ func init() {
 	r := http.NewServeMux()
 
 	r.HandleFunc("GET /api/health", health)
-	r.HandleFunc("POST /api/register", registerUser)
-	r.HandleFunc("POST /api/login", loginUser)
+	r.HandleFunc("POST /api/register", RegisterUser)
+	r.HandleFunc("POST /api/login", LoginUser)
 	r.HandleFunc("GET /api/protected", protectedRoute)
 
 	Serve = r
@@ -31,104 +28,6 @@ func health(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
-}
-
-func registerUser(w http.ResponseWriter, req *http.Request) {
-	// parse user
-	newUser, err := utils.DecodeUserJSON(req.Body)
-	if err != nil {
-		message := "Error parsing user"
-		response, _ := utils.ParseResponse(message)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(response) // Write the JSON data to the response body
-		return
-	}
-	log.Print(newUser)
-
-	// add user
-	err = services.CreateUser(newUser)
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-		return
-	}
-
-	response, _ := utils.ParseResponse("user created")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(response) // Write the JSON data to the response body
-}
-
-func loginUser(w http.ResponseWriter, req *http.Request) {
-
-	var loggedUser struct {
-		Email    string
-		Password string
-	}
-
-	decoder := json.NewDecoder(req.Body)
-	if err := decoder.Decode(&loggedUser); err != nil {
-		message := "Error parsing user"
-		response, _ := utils.ParseResponse(message)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(response)
-		return
-	}
-
-	// get the user from database
-	dbUser, err := services.GetUserByEmail(loggedUser.Email)
-	if err != nil {
-		message := string(err.Error())
-		response, _ := utils.ParseResponse(message)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(response) // Write the JSON data to the response body
-		return
-	}
-
-	// match the passwords
-	if loggedUser.Password != dbUser.Password {
-		fmt.Print(dbUser, loggedUser)
-		message := "incorrect password"
-		response, _ := utils.ParseResponse(message)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(response) // Write the JSON data to the response body
-		return
-	}
-
-	// create jwt token
-	token, err := utils.CreateToken(dbUser.Email)
-	if err != nil {
-		message := "error generating token"
-		response, _ := utils.ParseResponse(message)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write(response)
-		fmt.Print(message)
-		return
-	}
-
-	// attach JWT TOKEN TO Response
-	cookie := http.Cookie{
-		Name:   "token",
-		Value:  token,
-		MaxAge: 3600,
-		Domain: "http://localhost:8080",
-		Path:   "/",
-		Secure: true, // Only send cookie over HTTPS connections (if applicable)
-	}
-	// Set the cookie in the response
-	http.SetCookie(w, &cookie)
-	log.Print(cookie)
-	resUser := struct {
-		Id    string `json:"id"`
-		Name  string `json:"name"`
-		Email string `json:"email"`
-		Role  string `json:"role"`
-	}{Id: dbUser.Id, Name: dbUser.Name, Email: dbUser.Email, Role: dbUser.Role}
-	response := models.Response{Message: "successful!", Data: resUser}
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		log.Print(err)
-		fmt.Fprintf(w, "internal error")
-		return
-	}
 }
 
 // FOR TESTING
---
+++ handlers/todo.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func GetTodo(w http.ResponseWriter, req *http.Request)    {}
+func GetAllTodo(w http.ResponseWriter, req *http.Request) {}
+func PostTodo(w http.ResponseWriter, req *http.Request)   {}
+func DeleteTodo(w http.ResponseWriter, req *http.Request) {}
