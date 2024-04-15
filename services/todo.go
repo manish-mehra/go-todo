62294905bf69a0e794f28cb57f7e19b0077f541b
manish-mehra <manishmehra1469@gmail.com>
2024-04-15 18:18:30 +0530

    feat: get todo

--- services/todo.go
+++ services/todo.go
@@ -10,6 +10,7 @@ import (
 
 var (
 	stmtPostTodo *sql.Stmt
+	stmtGetTodo  *sql.Stmt
 )
 
 func init() {
@@ -17,6 +18,11 @@ func init() {
 	var err error
 	// prepare posttodo statement
 	stmtPostTodo, err = mysqlDB.Prepare("INSERT INTO Todo (title, completed, user_id) VALUES (?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Select * From Todo Where user_id = 1231 AND id = 4;
+	stmtGetTodo, err = mysqlDB.Prepare("SELECT id, title, completed FROM Todo WHERE user_id = ? AND id = ?")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,4 +34,19 @@ func PostTodo(todo models.UserTodo, userId int64) error {
 		return errors.New("failed to post todo")
 	}
 	return nil
+}
+
+// GetTodo return Todo & error
+// Expect userId and id(todo) as arg
+func GetTodo(userId int64, id int64) (models.Todo, error) {
+	var todo models.Todo
+	err := stmtGetTodo.QueryRow(userId, id).Scan(&todo.ID, &todo.Title, &todo.Completed)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// No todo found with the provided user ID and ID
+			return models.Todo{}, errors.New("todo not found")
+		}
+		return models.Todo{}, errors.New("failed to get todo: " + err.Error())
+	}
+	return todo, nil
 }
